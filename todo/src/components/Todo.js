ed9bd22ffa5aadb18d339dfa106a01b5b1bce4fa
unknown <will.ediger@gmail.com>
2019-04-15 02:12:57 -0500

    modified todo to use redux dispatch prop for togglecompletion

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './Todo.css';
+import React from "react";
+import "./Todo.css";
 
 const Todo = props => {
-  const classes = ['item'];
-  if (props.task.completed) {classes.push('completed')}
-  if (props.task.filtered) {classes.push('filtered')}
+  const classes = ["item"];
+  if (props.task.completed) {
+    classes.push("completed");
+  }
+  if (props.task.filtered) {
+    classes.push("filtered");
+  }
   return (
-    <div 
-      className={classes.join(' ')} 
-      onClick={props.toggleTodoCompletion(props.task.id)}
+    <div
+      className={classes.join(" ")}
+      onClick={() => props.toggleTodoCompletion(props.task.id)}
     >
-      {props.task.taskName}
+      {props.task.task}
     </div>
   );
-}
+};
 
-
-
-export default Todo
+export default Todo;
